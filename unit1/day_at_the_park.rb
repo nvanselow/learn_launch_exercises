62b958a892902341505634bc9b276b25e867bb02
Nick(Mac) <nvanselow@hotmail.com>
2015-09-03 23:23:10 -0400

    Unit 1: Day at the park

---
+++ unit1/day_at_the_park.rb
@@ -0,0 +1,13 @@
+# variables
+size = "large"
+screamer = "Jenn"
+color = "blue"
+coldness = "very"
+ride_type = "crazy"
+
+#phrases
+puts "What SIZE sundae did you get? \n I got a #{size} sundae and it was delicious."
+puts "WHO screamed the loudest on a ride? \n #{screamer} screamed the loudest on the ride."
+puts "What COLOR of cotton candy was the most delicious? \n The #{color} cotton candy was the best!"
+puts "HOW COLD was the water in the wave pool? \n The water was #{coldness} cold today."
+puts "Were your favorite rides CRAZY OR CALM? \n My favorite rides were #{ride_type}."
