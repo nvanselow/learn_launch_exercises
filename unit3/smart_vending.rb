fe9e9a5582d579a3382a5f548c947fd8628c2205
Nick(Mac) <nvanselow@hotmail.com>
2015-09-07 14:23:45 -0400

    Smart vending part 2

--- unit3/smart_vending.rb
+++ unit3/smart_vending.rb
@@ -4,13 +4,20 @@ puts "What would you like?"
 item = gets.chomp.to_s
 
 puts "How many would you like?"
-quantity = gets.chomp.to_i
+quantity = gets.chomp
 
-quantity.times do
-  puts item
+if quantity.to_s == "tons"
+  until quantity == 0 do
+    puts item
+    quantity = rand(0..14)
+  end
+else
+  quantity.to_i.times do
+    puts item
+  end
 end
 
-if quantity <= 0
+if quantity <= 0 && quantity != 'tons'
   puts "If you didn't want any. Why did you ask? (Try entering a number greater than 0)"
 else
   puts "There you go. Visit again soon!"
